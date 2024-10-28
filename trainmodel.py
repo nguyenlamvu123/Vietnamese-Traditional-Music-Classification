7aadfde103193cbc60fa8614684746721fa088c3
Nguyen Lam Vu <nguyenlamvu123@gmail.com>
2024-10-28 10:56:53 +0700

    - train lại mô hình với 4 nhãn là thiếu nhi, cách mạng, nhạc trẻ, trữ tình; độ chính xác được 60%
    - để cho người dùng lựa chọn 'đánh giá' hay 'huấn luyện' khi chạy trainmodel.py

--- trainmodel.py
+++ trainmodel.py
@@ -4,7 +4,12 @@ from config import TRAIN_ROOT, VAL_ROOT, TEST_ROOT
 
 if __name__ == "__main__":
     train_generator, val_generator, test_generator = generate_data(TRAIN_ROOT, VAL_ROOT, TEST_ROOT)
-    # # Can change index from 1 to 3, or 0
-    # best_model = train_model(0, train_set=train_generator, val_set=val_generator, test_set=test_generator)
-    for model_index in range(3):
-        best_model = load_best_model(model_index=model_index + 1, val_set=val_generator)
+    mod = input("nhập tùy chọn 'huấn luyện' hoặc 'đánh giá'")
+    if mod == "huấn luyện":
+        # Can change index from 1 to 3, or 0
+        best_model = train_model(0, train_set=train_generator, val_set=val_generator, test_set=test_generator)
+    elif mod == "đánh giá":
+        for model_index in range(3):
+            best_model = load_best_model(model_index=model_index + 1, val_set=val_generator)
+    else:
+        raise Exception("không có tùy chọn khác ngoài 'huấn luyện' và 'đánh giá'")
